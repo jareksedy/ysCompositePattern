71ee915a7b74403792cc494108b576c84ccb6a5f
Jarek <jareksedy@icloud.com>
2021-10-04 00:06:45 +0600

    Adding root level tasks.

--- ysCompositePattern/CompositePattern/CompositePattern.swift
+++ ysCompositePattern/CompositePattern/CompositePattern.swift
@@ -6,19 +6,22 @@
 //
 
 import Foundation
+import SwiftUI
 
 protocol TaskComposite {
+    var id: UUID { get }
     var parent: TaskComposite? { get }
-    var children: [TaskComposite] { get }
+    var children: [Task] { get set }
     var name: String { get }
     
-    func add(child: TaskComposite)
+    func add(child: Task)
     func listChildren() -> [String]
 }
 
-class Task: TaskComposite {
+class Task: TaskComposite, Identifiable, ObservableObject {
+    let id = UUID()
     var parent: TaskComposite?
-    var children: [TaskComposite] = []
+    @Published var children: [Task] = []
     var name: String
     
     init(name: String, parent: TaskComposite?) {
@@ -26,7 +29,7 @@ class Task: TaskComposite {
         self.parent = parent
     }
     
-    func add(child: TaskComposite) {
+    func add(child: Task) {
         children.append(child)
     }
     
--- ysCompositePattern/ContentView.swift
+++ ysCompositePattern/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var myTaskTree = Task(name: "My tasks", parent: nil)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(myTaskTree.children) { child in
+                    Text(child.name).padding()
+                }
+            }
+            .listStyle(.plain)
+            .navigationBarTitle(myTaskTree.name).padding()
+            .toolbar {
+                Button("Add task") {
+                    textAlert() { userInput in
+                        guard userInput != "" else { return }
+                        myTaskTree.add(child: Task(name: userInput, parent: myTaskTree))
+                    }
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().preferredColorScheme(.dark)
     }
 }
+
