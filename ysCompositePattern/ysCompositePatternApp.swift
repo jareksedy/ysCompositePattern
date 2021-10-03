d0e1ac6835acf25882db27aa67b81fcef5cd734e
Jarek <jareksedy@icloud.com>
2021-10-03 21:45:10 +0600

    Initial Commit

---
+++ ysCompositePattern/ysCompositePatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  ysCompositePatternApp.swift
+//  ysCompositePattern
+//
+//  Created by Ярослав on 03.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct ysCompositePatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
